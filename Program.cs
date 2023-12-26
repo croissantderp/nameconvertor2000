95ca2d499c703eb011fd223c57196a08896c1f3b
croissantderp <bookish.gecko@gmail.com>
2023-12-25 23:08:45 -0500

    Initial commit the second

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace nameconvertor2000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using StreamReader file = new(@"D:\Documents\wd.txt");
+            string enter = file.ReadToEnd().Replace("\n", "\" , \"").Replace("\r", "");
+
+            using StreamWriter file2 = new(@"D:\Documents\wd2.txt");
+            file2.Write("\"" + enter + "\"");
+
+            Console.WriteLine("done");
+        }
+    }
+}
